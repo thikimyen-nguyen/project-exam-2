039b2f3b3cb6dc48dbf77cd86176235a3432f28f
Yen Nguyen <nguyenthikimyen25@gmail.com>
2024-04-29 22:30:57 +0200

    fetch current profile

--- src/store/profile.jsx
+++ src/store/profile.jsx
@@ -1,63 +1,7 @@
 import { create } from "zustand";
 import { apiKeyUrl, singleProfileUrl } from "../api";
 
-// const useProfileStore = create((set, get) => ({
-//   currentProfile: {},
-//   isLoading: false,
-//   isError: false,
-//   apiKey: {},
-//   fetchApiKey: async (accessToken) => {
-//     set({ isLoading: true, isError: false });
-//     try {
-//       const postOption = {
-//         method: "POST",
-//         headers: {
-//             Authorization: `Bearer ${accessToken}`
-            
-//           }
-        
-//       };
-//       const response = await fetch(apiKeyUrl, postOption);
-//       const json = await response.json();
-//       console.log(json.data);
-//       set((state) => ({ ...state, apiKey: json.data }));
 
-
-//     } catch (error) {
-//       set({ isError: true });
-//     } finally {
-//       set({ isLoading: false });
-//     }
-//   },
-//   fetchSingleProfile: async (userName, apiKey, accessToken) => {
-//     set({ isLoading: true, isError: false });
-//     try {
-//       const postOption = {
-//         method: "GET",
-//         headers: {
-//             Authorization: `Bearer ${accessToken}`,
-//             "X-Noroff-API-Key": apiKey.key
-//           }
-       
-//       };
-//       const url = {singleProfileUrl}/{userName};
-//       const response = await fetch(url, postOption);
-//       const json = await response.json();
-//       console.log(url);
-//       set((state) => ({ ...state, currentProfile: json.data }));
-     
-      
-
-//     } catch (error) {
-//       set({ isError: true });
-//     } finally {
-//       set({ isLoading: false });
-//     }
-//   },
-  
-// }));
-
-// export default useProfileStore;
 
 const useProfileStore = create((set, get) => ({
     currentProfile: {},
